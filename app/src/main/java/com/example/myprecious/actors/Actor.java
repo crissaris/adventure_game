8e69e7007931b4f8db671e9e85c5ca9a193b2f87
Aristarchos <valeripagian@gmail.com>
2024-10-17 21:45:16 +0300

    version 1.4 create few actors

--- app/src/main/java/com/example/myprecious/actors/Actor.java
+++ app/src/main/java/com/example/myprecious/actors/Actor.java
@@ -1,4 +1,166 @@
 package com.example.myprecious.actors;
 
+import com.example.myprecious.R;
+import com.example.myprecious.containers.Container;
+import com.example.myprecious.items.Item;
+import com.example.myprecious.items.MagicalItem;
+
 public class Actor {
+    private String name;
+    private String lastname;
+    private String alias;
+    private String race;
+    private Item ring;
+    private Item right_hand;
+    private Item left_hand;
+    private Container container;
+    private int bust_image;
+    private int full_body_image;
+    private int power;
+    private int health;
+
+    public Actor() {
+    }
+
+    public Actor(String name, String lastname, String alias, String race,
+                 Item ring, Item right_hand, Item left_hand, Container container,
+                 int bust_image, int full_body_image, int power, int health) {
+        this.name = name;
+        this.lastname = lastname;
+        this.alias = alias;
+        this.race = race;
+        this.ring = ring;
+        this.right_hand = right_hand;
+        this.left_hand = left_hand;
+        this.container = container;
+        this.bust_image = bust_image;
+        this.full_body_image = full_body_image;
+        this.power = power;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public Item getRing() {
+        return ring;
+    }
+
+    public void setRing(Item ring) {
+        this.ring = ring;
+    }
+
+    public Item getRight_hand() {
+        return right_hand;
+    }
+
+    public void setRight_hand(Item right_hand) {
+        this.right_hand = right_hand;
+    }
+
+    public Item getLeft_hand() {
+        return left_hand;
+    }
+
+    public void setLeft_hand(Item left_hand) {
+        this.left_hand = left_hand;
+    }
+
+    public Container getContainer() {
+        return container;
+    }
+
+    public void setContainer(Container container) {
+        this.container = container;
+    }
+
+    public int getBust_image() {
+        return bust_image;
+    }
+
+    public void setBust_image(int bust_image) {
+        this.bust_image = bust_image;
+    }
+
+    public int getFull_body_image() {
+        return full_body_image;
+    }
+
+    public void setFull_body_image(int full_body_image) {
+        this.full_body_image = full_body_image;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    // actors...
+    public static class FrodoBaggins extends Actor {
+        public FrodoBaggins() {
+            super("Frodo", "Baggins", "ringbearer",
+                    "Hobbit", new Item(), new Item(), new Item(),
+                    new Container(), R.drawable.frodo_face,
+                    R.drawable.frodo_baggins, 100, 300);
+        }
+    }
+    public static class GaladrielAlatariel extends Actor {
+        public GaladrielAlatariel() {
+            super("Galadriel", "Alatariel", "Lady of Lórien",
+                    "Elves",new MagicalItem.Nenya(), new Item(), new Item(), new Container(),
+                    R.drawable.galadriel_face,R.drawable.galadriel,500, 800);
+        }
+    }
+    public static class GandalfOlórin extends Actor {
+        public GandalfOlórin() {
+            super("Gandalf", "Olórin", "Mithrandir", "Maiar", new MagicalItem.Narya(),
+                    new Item(), new Item(), new Container(), R.drawable.gandalf_face,
+                    R.drawable.gandalf, 500, 1000);
+        }
+    }
+    public static class AragornSonOfArathorn extends Actor {
+        public AragornSonOfArathorn() {
+            super("Aragorn", "Ranger of the North", "strider", "Men", new MagicalItem.RingOfBarahir(),
+                    new Item(), new Item(), new Container(),R.drawable.aragorn_face, R.drawable.aragorn, 300, 500);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/myprecious/items/MagicalItem.java
@@ -0,0 +1,63 @@
+package com.example.myprecious.items;
+
+import com.example.myprecious.R;
+
+public class MagicalItem extends Item {
+    private String description;
+    private int magical_power;
+
+    public MagicalItem(String description, int amount, int image, String description1, int magical_power) {
+        super(description, amount, image);
+        this.description = description1;
+        this.magical_power = magical_power;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMagical_power() {
+        return magical_power;
+    }
+
+    public void setMagical_power(int magical_power) {
+        this.magical_power = magical_power;
+    }
+    // rings...
+    // galadriel...
+    public static class Nenya extends MagicalItem {
+        public Nenya() {
+            super("Nenya", 1, R.drawable.nenya, "the Ring of Adamant", 200);
+        }
+    }
+    // gandalf...
+    public static class Narya extends MagicalItem {
+        public Narya() {
+            super("Narya", 1, R.drawable.narya, " the Ring of Fire", 200);
+        }
+    }
+    // elrond...
+    public static class Vilya extends MagicalItem {
+        public Vilya() {
+            super("Vilya", 1, R.drawable.vylia, "the Ring of Air", 200);
+        }
+    }
+    // aragorn
+    public static class RingOfBarahir extends MagicalItem {
+        public RingOfBarahir() {
+            super("ring of Barahir", 1, R.drawable.ring_of_barahir, "ring of Barahir", 50);
+        }
+    }
+    //frodo
+    public static class OneRing extends MagicalItem {
+        public OneRing() {
+            super("precious", 1, R.drawable.one_ring, "the one Ring", 600);
+        }
+    }
+}
